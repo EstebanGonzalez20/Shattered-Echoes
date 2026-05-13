3ee6859d82bfe512692da3cc992d62100b07daa2
Santino7537 <martinez.santinonicolaset36@gmail.com>
2026-05-13 10:52:16 -0300

    Refacciona 'attack_util' para separar sus datos

---
+++ Engine/Utils/attack_data.gd
@@ -0,0 +1,11 @@
+extends Resource
+class_name AttackData
+
+@export var shot_speed: float
+@export var duration: float
+
+@export var num_operates_damage: float = 1.0
+@export var num_operates_attack_speed: float = 1.0
+
+@export var damage_operation: String = "mul"
+@export var attack_speed_operation: String = "mul"
--- Engine/Utils/attack_util.gd
+++ Engine/Utils/attack_util.gd
@@ -1,26 +1,18 @@
 extends Area3D
-
 class_name Attack
 
-@export var shot_speed: float
-@export var duration: float
-
-@export var num_operates_damage: float = 1.0
-@export var num_operates_attack_speed: float = 1.0
-
-@export var damage_operation: String = "mul"
-@export var attack_speed_operation: String = "mul"
+@export var attack_data: AttackData
 
 var damage_function: Callable
 var attack_speed_function: Callable
 
 func _ready():
 	damage_function = MathScaling.scaling_function(
-		num_operates_damage,
-		damage_operation
+		attack_data.num_operates_damage,
+		attack_data.damage_operation
 	)
 
 	attack_speed_function = MathScaling.scaling_function(
-		num_operates_attack_speed,
-		attack_speed_operation
+		attack_data.num_operates_attack_speed,
+		attack_data.attack_speed_operation
 	)
