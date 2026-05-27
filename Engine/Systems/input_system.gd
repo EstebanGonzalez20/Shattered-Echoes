4c0fa26e6e2b158f709d27faf359bcd8a4785af4
asansantiago316 <asan.santiagoet36@gmail.com>
2026-05-27 16:18:05 -0300

    se crearon los inputs de basic attack y special attack

--- Engine/Systems/input_system.gd
+++ Engine/Systems/input_system.gd
@@ -4,8 +4,13 @@ extends Node
 func process_actions(input_comp: InputComponent):
 	input_comp.move_vector = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	input_comp.jump_pressed = Input.is_action_just_pressed("ui_accept")
+	
 
 
-func process_mouse(event: InputEvent, input_comp: InputComponent):
+func process_mouse(event: InputEvent, input_comp: InputComponent, weapon_comp: WeaponComponent, player):
 	if event is InputEventMouseMotion:
 		input_comp.mouse_delta = event.relative
+	if Input.is_action_pressed("basic_attack"):
+		AttackSystem.perform_attack(player, weapon_comp.basic_attack)
+	if Input.is_action_pressed("special_attack"):
+		AttackSystem.perform_attack(player, weapon_comp.special_attack)
