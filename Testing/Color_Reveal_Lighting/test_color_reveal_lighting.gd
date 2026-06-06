93050d3c1255437dfd0e2f0959d1d671a01f1f5a
Santino7537 <martinez.santinonicolaset36@gmail.com>
2026-06-05 23:48:26 -0300

    Crea shaders para revelar colores de objetos 'noir'.
    - Crea una escena de testeo para probar el shader de revelado de colores.
    - Crea un archivo de shader para lograr el revelado de colores.
    - Crea un grupo para crear luces que revelen colores.

---
+++ Testing/Color_Reveal_Lighting/test_color_reveal_lighting.gd
@@ -0,0 +1,46 @@
+extends Node3D
+
+var MAX_LIGHTS: int = 64
+var light_image: Image
+var light_texture: ImageTexture
+
+@onready var player = $Player
+
+func _ready() -> void:
+	light_image = Image.create(MAX_LIGHTS, 1, false, Image.FORMAT_RGBAF)
+	light_texture = ImageTexture.create_from_image(light_image)
+	RenderingServer.global_shader_parameter_add(
+		"color_lights_tex",
+		RenderingServer.GLOBAL_VAR_TYPE_SAMPLER2D,
+		light_texture
+	)
+	RenderingServer.global_shader_parameter_add(
+		"color_light_count",
+		RenderingServer.GLOBAL_VAR_TYPE_INT,
+		0
+	)
+
+	for enemy_mesh in get_tree().get_nodes_in_group("enemies"):
+		var mat: ShaderMaterial = ShaderMaterial.new()
+		mat.shader = preload("res://Game/Shaders/color_reveal_lighting.gdshader")
+		mat.set_shader_parameter("albedo_texture", preload("res://Testing/Color_Reveal_Lighting/color_red.png"))
+		enemy_mesh.get_node("MeshInstance3D").material_override = mat
+
+func _process(_delta: float) -> void:
+	var lights: Array[Node] = get_tree().get_nodes_in_group("color_revealer")
+	var count: int = min(lights.size(), MAX_LIGHTS)
+
+	for i in count:
+		var l = lights[i] as OmniLight3D
+		var pos = l.global_position
+		light_image.set_pixel(i, 0, Color(pos.x, pos.y, pos.z, l.omni_range))
+
+	for i in range(count, MAX_LIGHTS):
+		light_image.set_pixel(i, 0, Color(0, -9999, 0, 0))
+
+	light_texture.update(light_image)
+	RenderingServer.global_shader_parameter_set("color_lights_tex", light_texture)
+	RenderingServer.global_shader_parameter_set("color_light_count", count)
+
+func _physics_process(delta: float) -> void:
+	get_tree().call_group("enemies", "update_target_location", player.global_transform.origin)
