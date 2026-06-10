98dd3c9219d32dece066566e5df8f5c9984a2c95
asansantiago316 <asan.santiagoet36@gmail.com>
2026-06-10 15:40:05 -0300

    arregle el movimiento de la camara, ahora se mueve independientemente del personaje en vez de moverlo por completo cuando miras para arriba o abajo

--- Engine/Systems/look_system.gd
+++ Engine/Systems/look_system.gd
@@ -1,13 +1,13 @@
 class_name LookSystem
 extends Node
 
-func process(entity: Node3D, look: LookComponent, input: InputComponent):
+func process(player: CharacterBody3D, camera_pivot: Node3D, look: LookComponent, input: InputComponent):
 	look.yaw -= input.mouse_delta.x * look.mouse_sens
 	look.pitch -= input.mouse_delta.y * look.mouse_sens
-	
+
 	look.pitch = clamp(look.pitch, -80, 80)
 
-	entity.rotation_degrees.y = look.yaw
-	entity.rotation_degrees.x = look.pitch
+	player.rotation_degrees.y = look.yaw
+	camera_pivot.rotation_degrees.x = look.pitch
 
 	input.mouse_delta = Vector2.ZERO
--- Game/Scripts/Characters/player.gd
+++ Game/Scripts/Characters/player.gd
@@ -44,7 +44,7 @@ func _input(event):
 
 func _physics_process(delta):
 	input_system.process_actions(input_comp)
-	look_system.process(self, look, input_comp)
+	look_system.process(self,$Head, look, input_comp)
 	
 	var parameters: MovementSystemParameters = MovementSystemParameters.new()
 	parameters.set_player_parameters(self, movement, input_comp)
